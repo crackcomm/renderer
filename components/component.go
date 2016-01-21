9241ed6aaa3d43a456d7640764f64d87f19a49c6
crackcomm <crackcomm@gmail.com>
2016-01-22 00:35:06 +0100

    components: json tags

--- components/component.go
+++ components/component.go
@@ -3,20 +3,20 @@ package components
 // Component - Component definition.
 type Component struct {
 	// Name - Name of the component as registered in global scope.
-	Name string
+	Name string `json:"name,omitempty"`
 
 	// Main - Main entrypoint of rendering the component.
-	Main string
+	Main string `json:"main,omitempty"`
 
 	// Extends - Parent of the component.
 	// Parent will be rendered with this component html as `children` in context.
-	Extends string
+	Extends string `json:"extends,omitempty"`
 
 	// Styles - List of relative paths or URLs to CSS files.
-	Styles []string
+	Styles []string `json:"styles,omitempty"`
 
 	// Scripts - List of relative paths or URLs to JS files.
-	Scripts []string
+	Scripts []string `json:"scripts,omitempty"`
 
 	// Require - Components required by this component.
 	// Those will be rendered before and set in context under keys from map.
@@ -32,8 +32,16 @@ type Component struct {
 // Rendered - Rendered component.
 type Rendered struct {
 	// Body - Body of the rendered component.
-	Body string
+	Body string `json:"body,omitempty"`
 
 	// Links - List of routes(/links) pointing to files.
-	Links map[string]string
+	Links map[string]string `json:"links,omitempty"`
+
+	// Styles - List of styles.
+	// They can be urls or list of css styles with prefix "data:text/css;".
+	Styles []string `json:"styles,omitempty"`
+
+	// Scripts - List of scripts.
+	// They can be urls or list of js scripts with prefix "data:text/javascript;".
+	Scripts []string `json:"scripts,omitempty"`
 }
