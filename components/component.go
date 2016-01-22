be4bbe13dbff710c6ffa3178856c6427065bdf69
crackcomm <crackcomm@gmail.com>
2016-01-22 23:37:56 +0100

    components: rendered

--- components/component.go
+++ components/component.go
@@ -30,20 +30,3 @@ type Component struct {
 	// With - Like context but values should be templates.
 	With map[string]string `json:"with,omitempty"`
 }
-
-// Rendered - Rendered component.
-type Rendered struct {
-	// Body - Body of the rendered component.
-	Body string `json:"body,omitempty"`
-
-	// Links - List of routes(/links) pointing to files.
-	Links map[string]string `json:"links,omitempty"`
-
-	// Styles - List of styles.
-	// They can be urls or list of css styles with prefix "data:text/css;".
-	Styles []string `json:"styles,omitempty"`
-
-	// Scripts - List of scripts.
-	// They can be urls or list of js scripts with prefix "data:text/javascript;".
-	Scripts []string `json:"scripts,omitempty"`
-}
