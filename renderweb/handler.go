a315b815149712de2701fcb83dccad229d6f2cde
Łukasz Kurowski <crackcomm@gmail.com>
2016-04-23 07:37:37 +0200

    move params back to template context

--- renderweb/handler.go
+++ renderweb/handler.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/rs/xhandler"
+	"github.com/rs/xmux"
 	"golang.org/x/net/context"
 
 	"github.com/crackcomm/renderer/components"
@@ -39,5 +40,6 @@ func (h *Handler) Construct(opts ...Option) (xhandler.HandlerC, error) {
 func initMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Request, next xhandler.HandlerC) {
 	ctx = NewRequestContext(ctx, r)
 	ctx = components.WithTemplateKey(ctx, "request", r)
+	ctx = components.WithTemplateKey(ctx, "params", xmux.Params(ctx))
 	next.ServeHTTPC(ctx, w, r)
 }
