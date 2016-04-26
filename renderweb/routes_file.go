2e55593225fe786b16f3191bd61a42160912cdeb
Łukasz Kurowski <crackcomm@gmail.com>
2016-04-26 18:33:58 +0200

    renderweb routes: no more map[interface{}]interface{}

--- renderweb/routes_file.go
+++ renderweb/routes_file.go
@@ -5,9 +5,11 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/crackcomm/renderer/middlewares"
-
 	"gopkg.in/yaml.v2"
+
+	"github.com/crackcomm/renderer/helpers"
+	"github.com/crackcomm/renderer/middlewares"
+	"github.com/crackcomm/renderer/options"
 )
 
 // RoutesFromFile - Reads routes from yaml file.
@@ -39,10 +41,42 @@ func (file routesFile) toRoutes() (routes Routes, err error) {
 				err = fmt.Errorf("middleware %q doesn't exist", m.Name)
 				return
 			}
+			if err := cleanMiddleware(m); err != nil {
+				return nil, fmt.Errorf("route %q: %v", r, err)
+			}
 		}
 		routes[route] = v
 	}
 	return
+}
+
+func cleanMiddleware(m *middlewares.Middleware) error {
+	// Clean middleware options
+	// because yaml gives us ugly maps map[interface{}]interface{}
+	// which is not we want in 99.99% of cases and this makes me
+	// write tons of unnecessary code to handle conversions
+	// lets convert it always here, JSON doesn't have this problem
+	v, ok := helpers.CleanMapDeep(map[string]interface{}(m.Options))
+	if !ok {
+		return fmt.Errorf("middleware %q invalid options", m.Name)
+	}
+
+	m.Options = options.Options(v.(map[string]interface{}))
+
+	// Clean templates map
+	v, ok = helpers.CleanMapDeep(map[string]interface{}(m.Template))
+	if !ok {
+		return fmt.Errorf("middleware %q invalid templates", m.Name)
+	}
+	m.Template = options.Options(v.(map[string]interface{}))
+
+	// Clean context map
+	v, ok = helpers.CleanMapDeep(map[string]interface{}(m.Context))
+	if !ok {
+		return fmt.Errorf("middleware %q invalid context", m.Name)
+	}
+	m.Context = options.Options(v.(map[string]interface{}))
+	return nil
 }
 
 func parseRoute(str string) (r Route, err error) {
