108372671591ab1236eb0c38534c9e8e2fc740f9
Łukasz Kurowski <crackcomm@gmail.com>
2016-04-28 08:14:46 +0200

    renderweb: middlewares tracing

--- renderweb/handler.go
+++ renderweb/handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rs/xhandler"
 	"github.com/rs/xmux"
 	"golang.org/x/net/context"
+	"golang.org/x/net/trace"
 
 	"tower.pro/renderer/components"
 	"tower.pro/renderer/middlewares"
@@ -20,10 +21,13 @@ type Handler struct {
 // Construct - Constructs http handler.
 func (h *Handler) Construct(opts ...Option) (xhandler.HandlerC, error) {
 	// Request initialization middleware
-	opts = append(opts, WithMiddleware(ToMiddleware(initMiddleware)))
+	opts = append(opts, WithMiddleware(initMiddleware))
 
 	// Set component-setting middleware with handler component
 	opts = append(opts, WithComponentSetter(ComponentMiddleware(h.Component)))
+
+	// Check if tracing is enabled
+	tracing := tracingEnabled(opts...)
 
 	// Construct handler middlewares
 	for _, md := range h.Middlewares {
@@ -31,15 +35,38 @@ func (h *Handler) Construct(opts ...Option) (xhandler.HandlerC, error) {
 		if err != nil {
 			return nil, err
 		}
+		if tracing {
+			middleware = tracingMiddleware(md, middleware)
+		}
 		opts = append(opts, WithMiddleware(middleware))
 	}
 
 	return New(opts...), nil
 }
 
-func initMiddleware(ctx context.Context, w http.ResponseWriter, r *http.Request, next xhandler.HandlerC) {
+var initMiddleware = ToMiddleware(func(ctx context.Context, w http.ResponseWriter, r *http.Request, next xhandler.HandlerC) {
 	ctx = NewRequestContext(ctx, r)
 	ctx = components.WithTemplateKey(ctx, "request", r)
 	ctx = components.WithTemplateKey(ctx, "params", xmux.Params(ctx))
 	next.ServeHTTPC(ctx, w, r)
+})
+
+// tracingMiddleware - Tracing for middlewares.
+func tracingMiddleware(md *middlewares.Middleware, handler middlewares.Handler) middlewares.Handler {
+	return func(next xhandler.HandlerC) xhandler.HandlerC {
+		h := handler(next)
+		return xhandler.HandlerFuncC(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+			tr, _ := trace.FromContext(ctx)
+			tr.LazyPrintf("%s", md.Name)
+			h.ServeHTTPC(ctx, w, r)
+		})
+	}
+}
+
+func tracingEnabled(opts ...Option) bool {
+	o := new(webOptions)
+	for _, opt := range opts {
+		opt(o)
+	}
+	return o.tracing
 }
